d54f5153ed7b274238700a8ea4ddcb08a56d4053
Lars Indresaeter <lars.indresaeter@gmail.com>
2026-06-24 22:09:25 +0200

    fixup git difftool

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-git config --global diff.tool nvimdiff
-git config --global difftool.cmd 'nvim -d $LOCAL $REMOTE'
+git config --global diff.tool vimdiff
+git config --global difftool.cmd 'vim -d $LOCAL $REMOTE'
 
-git config --global merge.tool nvimdiff2
-git config --global difftool2.cmd "nvim -d \$LOCAL $REMOTE \$MERGED -c '$wincmd w' -c 'wincmd J':"
+git config --global merge.tool vimdiff2
+git config --global difftool2.cmd "vim -d \$LOCAL $REMOTE \$MERGED -c '$wincmd w' -c 'wincmd J':"
